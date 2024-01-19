88b4bf673f0a997651af34b5ab7fb7b780552683
damien <doute.d@gmail.com>
2024-01-19 16:48:20 -0500

    WIP implementing agent mention manually...

--- webcomponents/src/elements/input-bar.ts
+++ webcomponents/src/elements/input-bar.ts
@@ -5,9 +5,11 @@ import {inputBarStyleTemplate} from "../styles";
 
 import "@ui5/webcomponents/dist/TextArea.js";
 import TextArea from "@ui5/webcomponents/dist/TextArea.js";
+import List from "@ui5/webcomponents/dist/List.js";
 //import {InputSuggestionText, SuggestionComponent} from "@ui5/webcomponents/dist/features/InputSuggestions";
 import SuggestionItem from "@ui5/webcomponents/dist/SuggestionItem";
 import {ProfilesZvm} from "@ddd-qc/profiles-dvm";
+import {AgentPubKeyB64} from "@holochain/client";
 //import SuggestionListItem from "@ui5/webcomponents/dist/SuggestionListItem";
 
 
@@ -31,10 +33,19 @@ export class InputBar extends LitElement {
 
   private _cacheInputValue: string = "";
 
+  private _mentionFilter: string = "";
+
+  @state() private _filteredAgents: AgentPubKeyB64[] = [];
+
 
   /** -- Gettters -- */
+
   get inputElem(): TextArea {
     return this.shadowRoot.getElementById("textMessageInput") as unknown as TextArea;
+  }
+
+  get suggestionListElem(): List {
+    return this.shadowRoot.getElementById("agent-list") as unknown as List;
   }
 
 
@@ -47,6 +58,11 @@ export class InputBar extends LitElement {
     if (inputBar) {
       inputBar.shadowRoot.appendChild(inputBarStyleTemplate.content.cloneNode(true));
     }
+
+    // if (this._filteredAgents.length > 0) {
+    //   console.log("Focuisng on SUGGESTION LIST")
+    //   this.suggestionListElem.focus();
+    // }
   }
 
 
@@ -68,8 +84,16 @@ export class InputBar extends LitElement {
               detail: this.inputElem.value, bubbles: true, composed: true
           }));
           this.inputElem.value = "";
+          this._filteredAgents = [];
+          this._cacheInputValue = "";
         }
       }
+    }
+
+    /** Undo filter if '@' has been erased */
+    if (e.keyCode == 8 && this.inputElem.value.substr(this.inputElem.value.length - 1) === "@") {
+      this._filteredAgents = [];
+      this._cacheInputValue = "";
     }
 
     /** Typed ' @' */
@@ -81,32 +105,43 @@ export class InputBar extends LitElement {
       //e.preventDefault();
       this._cacheInputValue = this.inputElem.value;
       //let suggestionItems = ["toto", "titi", "bob", "joe"];
-      let suggestionItems = this.profilesZvm ? this.profilesZvm.getNames() : [];
+      let suggestionItems = this.profilesZvm ? Object.entries(this.profilesZvm.perspective.profiles) : [];
       /** Filter */
-      const filtered = suggestionItems.filter((item) => {
-          return item.toUpperCase().indexOf(e.key.toUpperCase()) === 0;
+      const filtered = suggestionItems.filter(([_agentKey, profile]) => {
+          return profile.nickname.toUpperCase().indexOf(e.key.toUpperCase()) === 0;
       });
       if (filtered.length != 0) {
-          suggestionItems = filtered;
+          this._filteredAgents = filtered.map(([key, _nick]) => key);
+          this.suggestionListElem.focus();
       }
-
-      suggestionItems.forEach((suggestion) => {
-          const li = document.createElement("ui5-suggestion-item") as unknown as SuggestionItem;
-          //li.icon = "world";
-          //li.additionalText = "explore";
-          //li.additionalTextState = "Success";
-          //li.description = "travel the world";
-          li.text = suggestion;
-        this.inputElem.appendChild(li as unknown as Node);
-          li
-      });
     }
   }
 
 
   /** */
   render() {
-    console.log("<threads-input-bar>.render()", this.profilesZvm, this.showHrlBtn);
+    console.log("<threads-input-bar>.render()", this._filteredAgents, this.showHrlBtn);
+
+    /** Render agent lists for mentions */
+    const agentItems = this._filteredAgents
+      .map((key) => {
+        const profile = this.profilesZvm.perspective.profiles[key];
+        if (!profile) return html``;
+        //return html`<div>${profile.nickname}</div>`;
+        return html`             
+          <ui5-li 
+                .image=${profile && profile.fields.avatar? profile.fields.avatar : ""}
+                @click=${(e) => {
+                  e.preventDefault();
+                    this._filteredAgents = [];
+                    // this.inputElem.value += profile.nickname
+                    this.inputElem.value = this._cacheInputValue + profile.nickname + " "
+                    this.inputElem.focus();
+                }}>
+          ${profile.nickname}
+          </ui5-li>`;
+      });
+
 
     /** render all */
     return html`
@@ -119,7 +154,10 @@ export class InputBar extends LitElement {
             <ui5-button design="Positive" icon="attachment" @click=${(e) => {
                 this.dispatchEvent(new CustomEvent('upload', {detail: null, bubbles: true, composed: true}));
             }}></ui5-button>
-            <ui5-textarea id="textMessageInput" 
+            <ui5-list id="agent-list" style="display: ${this._filteredAgents.length > 0? "block" : "none"}" autofocus=${this._filteredAgents.length > 0? "true" : "false"}>
+                ${agentItems}
+            </ui5-list>            
+            <ui5-textarea id="textMessageInput" mode="SingleSelect"
                           placeholder="Message #${this.topic}"
                           growing
                           growing-max-lines="3"
@@ -137,6 +175,13 @@ export class InputBar extends LitElement {
   static get styles() {
     return [
       css`
+          #agent-list {
+            position: absolute;
+            z-index: 1;
+            bottom: 55px;
+            width: 400px;
+            box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+          }
           #inputBar {
             margin:10px;
             width: auto;
