31764b2ac2b9afb67b94120ef80491ace63a4f3d
damien <doute.d@gmail.com>
2024-06-26 09:44:51 -0400

    better install-submodules.sh

--- scripts/install-submodules.sh
+++ scripts/install-submodules.sh
@@ -14,22 +14,23 @@ fi
 
 hcversion=$1
 echo for holochain version $hcversion
-
 if [ "$hcversion" == "hc" ] || [ "$hcversion" == "" ] ; then
   echo Missing \"hc-version\" field in \"package.json\".
   exit 1
 fi
+hdkversion=hdk-${hcversion:2}
+echo Getting branch: $hdkversion
 
 echo \* Create 'submodules' folder
 rm -rf submodules
 mkdir submodules
 cd submodules
 
-echo \* Download Files repo: WARNING HARDCODED branch name
-git clone -b hdk-3.1 --depth 1 https://github.com/lightningrodlabs/files.git
+echo \* Download Files repo
+git clone -b $hdkversion --depth 1 https://github.com/lightningrodlabs/files.git
 
-echo \* Download zome-signals repo: WARNING HARDCODED branch name
-git clone -b hdk-3.1 --depth 1 https://github.com/ddd-mtl/zome-signals.git
+echo \* Download zome-signals repo
+git clone -b $hdkversion --depth 1 https://github.com/ddd-mtl/zome-signals.git
 
 echo \* Download latest install scripts
 git clone --depth 1 https://github.com/ddd-mtl/hc-prebuilt
