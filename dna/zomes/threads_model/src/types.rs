5c387f1a502dbe6eab8b1a3f9a37708e7aec00ed
damien <doute.d@gmail.com>
2024-12-02 11:35:32 -0500

    beadType

--- dna/zomes/threads_model/src/types.rs
+++ dna/zomes/threads_model/src/types.rs
@@ -7,7 +7,7 @@ pub struct BeadLink {
   pub creation_time: Timestamp, // index_time can be determined by rounding creation_time
   pub author: AgentPubKey,
   pub bead_ah: ActionHash,
-  pub bead_type: String,
+  pub bead_type: BeadType,
 }
 
 
