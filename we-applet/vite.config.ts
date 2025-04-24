b13c4affec1bda9d7c3b38e4bfaea891e41c9edf
damien <doute.d@gmail.com>
2025-04-24 12:50:37 -0400

    working devtest:we with vite

--- we-applet/vite.config.ts
+++ we-applet/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import checker from 'vite-plugin-checker';
-import dts from 'vite-plugin-dts';
-//import { viteStaticCopy } from 'vite-plugin-static-copy'
+//import dts from 'vite-plugin-dts';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 console.log("vite: process.env.HC_APP_PORT: ", process.env.HC_APP_PORT);
 console.log("vite: process.env.HAPP_BUILD_MODE: ", process.env.HAPP_BUILD_MODE);
@@ -11,11 +12,15 @@ const HAPP_BUILD_MODE = process.env.HAPP_BUILD_MODE? process.env.HAPP_BUILD_MODE
 console.log("vite: process.env.APPLET_VIEW: ", process.env.APPLET_VIEW);
 const APPLET_VIEW = process.env.APPLET_VIEW? process.env.APPLET_VIEW : "main";
 
-const DIST_FOLDER = "dist"
+const DIST_FOLDER = "."
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {},
+  resolve: {
+    alias: {
+      '@vines/app': path.resolve(__dirname, '../webapp/src')
+    }
+  },
   plugins: [
     checker({
       typescript: true,
@@ -23,7 +28,19 @@ export default defineConfig({
       //   lintCommand: 'eslint --ext .ts,.html . --ignore-path .gitignore',
       // },
     }),
-    dts(),
+    viteStaticCopy({
+      targets: [
+        { src: "weave.config.json", dest: DIST_FOLDER },
+        { src: "../webapp/favicon.ico", dest: DIST_FOLDER },
+        { src: "../webapp/icon.png", dest: DIST_FOLDER },
+        { src: "../webapp/add-to-pocket.svg", dest: DIST_FOLDER },
+        { src: "../webapp/logo.svg", dest: DIST_FOLDER },
+        { src: "../node_modules/@shoelace-style/shoelace/dist/themes/light.css", dest: DIST_FOLDER, rename: "styles.css" },
+        //{ src: '../node_modules/@shoelace-style/shoelace', dest: DIST_FOLDER }
+        { src: '../node_modules/@shoelace-style/shoelace/dist/assets', dest: DIST_FOLDER }
+      ]
+    }),
+    //dts(),
   ],
   define: {
     '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
@@ -34,18 +51,17 @@ export default defineConfig({
     "process.env.HC_ADMIN_PORT": JSON.stringify(process.env.HC_ADMIN_PORT) || undefined,
     'process.env.NO_WE': JSON.stringify(process.env.NO_WE || false),
   },
-  build: {
-    emptyOutDir: true,
-    minify: false,
-    outDir: DIST_FOLDER,
-    rollupOptions: {
-      output: {
-        entryFileNames: "index.js",
-        chunkFileNames: `assets/index-chunk.js`,
-        assetFileNames: "assets[extname]",
-      },
-    }
-  },
+  // build: {
+  //   emptyOutDir: true,
+  //   outDir: DIST_FOLDER,
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: "index.js",
+  //       chunkFileNames: `assets/index-chunk.js`,
+  //       assetFileNames: "assets[extname]",
+  //     },
+  //   }
+  // },
   server: {
     open: true // This will open the browser automatically
   }
