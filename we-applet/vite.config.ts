e6f34a38c9b9c271100afbba0f9f1739d2cf0b58
ddd-mtl <doute.d@gmail.com>
2025-10-13 16:49:47 -0400

    remove cross-env dependency and clean up scripts

--- we-applet/vite.config.ts
+++ we-applet/vite.config.ts
@@ -1,68 +1,72 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import path from 'path';
 import checker from 'vite-plugin-checker';
 //import dts from 'vite-plugin-dts';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 console.log("vite: process.env.HC_APP_PORT: ", process.env.HC_APP_PORT);
 console.log("vite: process.env.HAPP_BUILD_MODE: ", process.env.HAPP_BUILD_MODE);
-const HAPP_BUILD_MODE = process.env.HAPP_BUILD_MODE? process.env.HAPP_BUILD_MODE : "Release";
+const HAPP_BUILD_MODE = process.env.HAPP_BUILD_MODE ? process.env.HAPP_BUILD_MODE : "Release";
 
 
 console.log("vite: process.env.APPLET_VIEW: ", process.env.APPLET_VIEW);
-const APPLET_VIEW = process.env.APPLET_VIEW? process.env.APPLET_VIEW : "main";
+const APPLET_VIEW = process.env.APPLET_VIEW ? process.env.APPLET_VIEW : "main";
 
 const DIST_FOLDER = "."
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@vines/app': path.resolve(__dirname, '../webapp/src')
+    resolve: {
+        alias: {
+            '@vines/app': path.resolve(__dirname, '../webapp/src')
+        }
+    },
+    plugins: [
+        checker({
+            typescript: true,
+            // eslint: {
+            //   lintCommand: 'eslint --ext .ts,.html . --ignore-path .gitignore',
+            // },
+        }),
+        viteStaticCopy({
+            targets: [
+                {src: "weave.config.json", dest: DIST_FOLDER},
+                {src: "../webapp/favicon.ico", dest: DIST_FOLDER},
+                {src: "../webapp/icon.png", dest: DIST_FOLDER},
+                {src: "../webapp/add-to-pocket.svg", dest: DIST_FOLDER},
+                {src: "../webapp/logo.svg", dest: DIST_FOLDER},
+                {
+                    src: "../node_modules/@shoelace-style/shoelace/dist/themes/light.css",
+                    dest: DIST_FOLDER,
+                    rename: "styles.css"
+                },
+                //{ src: '../node_modules/@shoelace-style/shoelace', dest: DIST_FOLDER }
+                {src: '../node_modules/@shoelace-style/shoelace/dist/assets', dest: DIST_FOLDER}
+            ]
+        }),
+        //dts(),
+    ],
+    define: {
+        '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+        'process.env.HAPP_BUILD_MODE': JSON.stringify(HAPP_BUILD_MODE),
+        'process.env.HAPP_ENV': HAPP_BUILD_MODE == "Debug" ? JSON.stringify("BrowserWe") : JSON.stringify("We"),
+        'process.env.APPLET_VIEW': JSON.stringify(APPLET_VIEW),
+        "process.env.HC_APP_PORT": JSON.stringify(process.env.HC_APP_PORT),
+        "process.env.HC_ADMIN_PORT": JSON.stringify(process.env.HC_ADMIN_PORT) || undefined,
+        'process.env.NO_WE': JSON.stringify(process.env.NO_WE || false),
+    },
+    // build: {
+    //   emptyOutDir: true,
+    //   outDir: DIST_FOLDER,
+    //   rollupOptions: {
+    //     output: {
+    //       entryFileNames: "index.js",
+    //       chunkFileNames: `assets/index-chunk.js`,
+    //       assetFileNames: "assets[extname]",
+    //     },
+    //   }
+    // },
+    server: {
+        open: true // This will open the browser automatically
     }
-  },
-  plugins: [
-    checker({
-      typescript: true,
-      // eslint: {
-      //   lintCommand: 'eslint --ext .ts,.html . --ignore-path .gitignore',
-      // },
-    }),
-    viteStaticCopy({
-      targets: [
-        { src: "weave.config.json", dest: DIST_FOLDER },
-        { src: "../webapp/favicon.ico", dest: DIST_FOLDER },
-        { src: "../webapp/icon.png", dest: DIST_FOLDER },
-        { src: "../webapp/add-to-pocket.svg", dest: DIST_FOLDER },
-        { src: "../webapp/logo.svg", dest: DIST_FOLDER },
-        { src: "../node_modules/@shoelace-style/shoelace/dist/themes/light.css", dest: DIST_FOLDER, rename: "styles.css" },
-        //{ src: '../node_modules/@shoelace-style/shoelace', dest: DIST_FOLDER }
-        { src: '../node_modules/@shoelace-style/shoelace/dist/assets', dest: DIST_FOLDER }
-      ]
-    }),
-    //dts(),
-  ],
-  define: {
-    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
-    'process.env.HAPP_BUILD_MODE': JSON.stringify(HAPP_BUILD_MODE),
-    'process.env.HAPP_ENV': HAPP_BUILD_MODE == "Debug"? JSON.stringify("BrowserWe") : JSON.stringify("We"),
-    'process.env.APPLET_VIEW': JSON.stringify(APPLET_VIEW),
-    "process.env.HC_APP_PORT": JSON.stringify(process.env.HC_APP_PORT),
-    "process.env.HC_ADMIN_PORT": JSON.stringify(process.env.HC_ADMIN_PORT) || undefined,
-    'process.env.NO_WE': JSON.stringify(process.env.NO_WE || false),
-  },
-  // build: {
-  //   emptyOutDir: true,
-  //   outDir: DIST_FOLDER,
-  //   rollupOptions: {
-  //     output: {
-  //       entryFileNames: "index.js",
-  //       chunkFileNames: `assets/index-chunk.js`,
-  //       assetFileNames: "assets[extname]",
-  //     },
-  //   }
-  // },
-  server: {
-    open: true // This will open the browser automatically
-  }
 });
--- webapp/src/globals.ts
+++ webapp/src/globals.ts
@@ -1,16 +1,14 @@
 /** -- HC_APP_PORT & friends -- */
 import {DEFAULT_THREADS_DEF} from "./happDef";
-import {HappBuildModeType} from "@ddd-qc/lit-happ";
-import {HAPP_BUILD_MODE} from "@ddd-qc/lit-happ/dist/globals";
-
+import {HappBuildModeType, HAPP_BUILD_MODE} from "@ddd-qc/lit-happ";
 
 export let HC_APP_PORT: number | undefined = undefined;
 export let HC_ADMIN_PORT: number | undefined = undefined;
 try {
-  HC_APP_PORT = Number(process.env.HC_APP_PORT);
-  HC_ADMIN_PORT = Number(process.env.HC_ADMIN_PORT);
-} catch (e:any) {
-  console.log("HC_APP_PORT not defined")
+    HC_APP_PORT = Number(process.env.HC_APP_PORT);
+    HC_ADMIN_PORT = Number(process.env.HC_ADMIN_PORT);
+} catch (e: any) {
+    console.log("HC_APP_PORT not defined")
 }
 
 console.log("      HAPP_ID =", DEFAULT_THREADS_DEF.id)
@@ -19,8 +17,8 @@ console.log("HC_ADMIN_PORT =", HC_ADMIN_PORT);
 
 /** Remove console.log() in Retail */
 if (HAPP_BUILD_MODE === HappBuildModeType.Retail) {
-  // console.log("console.log() disabled");
-  // console.log = () => {};
-  console.log("console.log() changed into console.debug()");
-  console.log = console.debug
+    // console.log("console.log() disabled");
+    // console.log = () => {};
+    console.log("console.log() changed into console.debug()");
+    console.log = console.debug
 }
