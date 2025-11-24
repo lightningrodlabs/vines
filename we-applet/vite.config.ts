eab704a8a624472c5109ff32a4c39775cd06d1ab
ddd-mtl <doute.d@gmail.com>
2025-11-24 16:08:07 -0500

    delete old logo

--- we-applet/vite.config.ts
+++ we-applet/vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
         {src: "../webapp/favicon.ico", dest: DIST_FOLDER},
         {src: "../webapp/icon.png", dest: DIST_FOLDER},
         {src: "../webapp/add-to-pocket.svg", dest: DIST_FOLDER},
-        {src: "../webapp/logo.svg", dest: DIST_FOLDER},
+        //{src: "../webapp/logo.svg", dest: DIST_FOLDER},
         {
           src: "../node_modules/@shoelace-style/shoelace/dist/themes/light.css",
           dest: DIST_FOLDER,
