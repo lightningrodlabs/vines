98e6598a68637fd2215dfd661a3508af1755cb6a
ddd-mtl <doute.d@gmail.com>
2026-03-20 12:02:58 -0400

    Commented code for handling holochain://setup-completed event and splashscreen

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 use holochain_types::prelude::*;
 use std::path::PathBuf;
 use tauri_plugin_holochain::{HolochainPluginConfig, HolochainExt, vec_to_locked};
-use tauri::{Listener};
+use tauri::{Listener, Manager, AppHandle, Runtime};
 use tauri_plugin_log::{Target, TargetKind};
 
 pub mod commands;
@@ -53,82 +53,110 @@ pub fn run() {
             HolochainPluginConfig::new(holochain_dir(), network_config(TARGET_ARC))
         ))
         .setup(|app| {
-            let handle = app.handle().clone();
-            let handle_fail = app.handle().clone();
+           let handle = app.handle().clone();
+           let handle_fail = app.handle().clone();
 
            //#[cfg(target_os = "android")]
            //android::log("VINES Hello from Rust!");
 
-            app.handle()
+           println!("Holochain plugin setup start");
+
+           app.handle()
               .listen("holochain://setup-failed", move |event| {
                  println!("Holochain setup failed: {:?}", event);
                  handle_fail.exit(1);
               });
            // app.handle()
-           //    .listen("holochain://setup-completed", move |_event| {
-           //       let handle = handle.clone();
+           //    .listen("holochain://setup-completed", move |event| {
+                 //println!("Holochain plugin setup completed: {:?}", event);
+                 let handle = handle.clone();
+                 tauri::async_runtime::block_on(async move {
+                    let Ok(admin_ws) = handle.clone().holochain().expect("Holochain failed to initialize").admin_websocket().await else {
+                       eprintln!("Failed to setup Holochain.");
+                       return;
+                    };
+                    let Ok(installed_apps) = admin_ws
+                       .list_apps(None)
+                       .await
+                       .map_err(|err| tauri_plugin_holochain::Error::ConductorApiError(err)) else {
+                       eprintln!("Failed to list installed apps.");
+                       return;
+                    };
+                    println!("Installed Vines apps: {}", installed_apps.len());
+                    let one_happ_mode = installed_apps.len() == 1 && installed_apps[0].status == AppStatus::Enabled;
 
-            let result: anyhow::Result<()> = tauri::async_runtime::block_on(async move {
-               let admin_ws = handle.holochain()?.admin_websocket().await?;
-               let installed_apps = admin_ws
-                  .list_apps(None)
-                  .await
-                  .map_err(|err| tauri_plugin_holochain::Error::ConductorApiError(err))?;
-                println!("Installed Vines apps: {}", installed_apps.len());
+                    let res = async {
+                       let main_window = if one_happ_mode {
+                              // Make sure app is enabled
+                              let main_app = installed_apps.into_iter().next().unwrap();
+                              println!("Only one app installed, loading it directly: {}", main_app.installed_app_id);
+                              if main_app.status != AppStatus::Enabled {
+                                 println!("Enabling app: {}", main_app.installed_app_id);
+                                 handle.holochain()?.holochain_runtime.enable_app(main_app.installed_app_id.clone()).await?;
+                              }
+                              //
+                              handle.clone().holochain()?.update_app_if_necessary(
+                                 String::from(main_app.installed_app_id.clone()),
+                                 happ_bundle()
+                              ).await?;
+                              // Load window
+                              handle.holochain()?
+                                 .main_window_builder(String::from("main"), true, Some(main_app.installed_app_id), /*Some(url)*/ None).await?
+                                 //.inner_size(360.,800.)
+                                 //.build()?;
+                       } else {
+                          {
+                             handle.holochain()?
+                                .main_window_builder(String::from("main"), true, None, None).await?
+                               //.main_window_builder(String::from("main"), true, None, Some(admin_url(true).await)).await?
+                               //.inner_size(360.,800.)
+                               //.build()?;
+                          }
+                           // single app mode
+                           // {
+                           //    handle
+                           //       .holochain()?
+                           //       .install_app(
+                           //          "vines".to_string(),
+                           //          happ_bundle(),
+                           //          None,
+                           //          None,
+                           //          None,
+                           //       )
+                           //       .await?;
+                           //    app.holochain()?
+                           //       .main_window_builder(String::from("main"), true, Some("vines".to_string()), None).await?
+                           //       .build()?;
+                           // }
+                       };
+                       #[cfg(desktop)]
+                       {
+                          main_window = main_window.title(String::from("Vines"));
+                       }
+                       main_window.build().map_err(|e| anyhow::anyhow!("{e:?}"))?;
+                       Ok::<(), anyhow::Error>(())
+                    }.await;
 
-               if installed_apps.len() == 1 && installed_apps[0].status == AppStatus::Enabled {
-                     // Make sure app is enabled
-                     let main_app = installed_apps.into_iter().next().unwrap();
-                     println!("Only one app installed, loading it directly: {}", main_app.installed_app_id);
-                     if main_app.status != AppStatus::Enabled {
-                        println!("Enabling app: {}", main_app.installed_app_id);
-                        app.holochain()?.holochain_runtime.enable_app(main_app.installed_app_id.clone()).await?;
-                     }
-                     //
-                     handle.holochain()?.update_app_if_necessary(
-                        String::from(main_app.installed_app_id.clone()),
-                        happ_bundle()
-                     ).await?;
-                     // Load window
-                     app.holochain()?
-                        .main_window_builder(String::from("main"), true, Some(main_app.installed_app_id), /*Some(url)*/ None).await?
-                        //.inner_size(360.,800.)
-                        .build()?;
-                  } else {
-                     {
-                         app.holochain()?
-                            .main_window_builder(String::from("main"), true, None, None).await?
-                            //.main_window_builder(String::from("main"), true, None, Some(admin_url(true).await)).await?
-                            //.inner_size(360.,800.)
-                            .build()?;
-                     }
-                     // single app mode
-                     // {
-                     //    handle
-                     //       .holochain()?
-                     //       .install_app(
-                     //          "vines".to_string(),
-                     //          happ_bundle(),
-                     //          None,
-                     //          None,
-                     //          None,
-                     //       )
-                     //       .await?;
-                     //    app.holochain()?
-                     //       .main_window_builder(String::from("main"), true, Some("vines".to_string()), None).await?
-                     //       .build()?;
-                     // }
-                  }
-               Ok(())
-            });
-
-            result?;
-
-            Ok(())
-        })
+                    match res {
+                       Ok(()) => {
+                           #[cfg(desktop)]
+                           {
+                              if let Some(splashscreen) = handle.get_webview_window("splashscreen") {
+                                 let _ = splashscreen.close();
+                              }
+                           }
+                       }
+                       Err(e) => {
+                           eprintln!("Failed to open main window: {:?}", e);
+                       }
+                    }
+                 }); // block_on
+                 Ok(())
+              })
+              //Ok(())
+           //}) // setup done
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-   //})
 }
 
 pub fn holochain_dir() -> PathBuf {
